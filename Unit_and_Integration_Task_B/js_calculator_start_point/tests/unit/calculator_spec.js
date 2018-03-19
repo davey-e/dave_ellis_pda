5a9b1dcae141ca46a1a72775644bf51832e968cb
Dave Ellis <dave@ellisnetmail.co.uk>
2018-03-19 12:11:25 +0000

    Added unit tests for calculator add, subtract, multiply and divide functions

--- Unit_and_Integration_Task_B/js_calculator_start_point/tests/unit/calculator_spec.js
+++ Unit_and_Integration_Task_B/js_calculator_start_point/tests/unit/calculator_spec.js
@@ -11,4 +11,37 @@ describe('calculator', function () {
     assert.equal(true, true)
   })
 
+  it('should be able to add a number to the previous total', function(){
+    const expected = 11
+    calculator.previousTotal = 6
+    calculator.add(5);
+    const actual = calculator.runningTotal;
+    assert.strictEqual(actual, expected);
+  })
+
+  it('should be able to subtract a number from the previous total', function(){
+    const expected = 4
+    calculator.previousTotal = 6
+    calculator.subtract(2);
+    const actual = calculator.runningTotal;
+    assert.strictEqual(actual, expected);
+  })
+
+  it('should be able to multiply the previous total number by a number', function(){
+    const expected = 12
+    calculator.previousTotal = 6
+    calculator.multiply(2);
+    const actual = calculator.runningTotal;
+    assert.strictEqual(actual, expected);
+  })
+
+  it('should be able to divide the previous total number by a number', function(){
+    const expected = 3
+    calculator.previousTotal = 6
+    calculator.divide(2);
+    const actual = calculator.runningTotal;
+    assert.strictEqual(actual, expected);
+  })
+
+
 });
