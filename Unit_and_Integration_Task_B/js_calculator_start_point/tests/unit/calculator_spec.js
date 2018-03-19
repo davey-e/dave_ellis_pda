49c8e979a2347ca9dbca896c898795f3b86adce7
Dave Ellis <dave@ellisnetmail.co.uk>
2018-03-19 13:45:59 +0000

    Added unit tests for calculator operatorClick function

--- Unit_and_Integration_Task_B/js_calculator_start_point/tests/unit/calculator_spec.js
+++ Unit_and_Integration_Task_B/js_calculator_start_point/tests/unit/calculator_spec.js
@@ -65,5 +65,103 @@ describe('calculator', function () {
     assert.strictEqual(actual, expected);
   })
 
+  it('should set newTotal to true when an operator button is clicked', function(){
+    const expected = true;
+    calculator.numberClick(6);
+    calculator.operatorClick('+');
+    const actual = calculator.newTotal;
+    assert.strictEqual(actual, expected);
+  })
+
+  it('should set the previousTotal value to be equal to the runningTotal value when an operator button is clicked', function(){
+    const expected = 6;
+    calculator.numberClick(6);
+    calculator.operatorClick('+');
+    const actual = calculator.previousTotal;
+    assert.strictEqual(actual, expected);
+  })
+
+  it('should perform the previous addition operation when an operator button is clicked', function(){
+    const expected = 11;
+    calculator.numberClick(6);
+    calculator.operatorClick('+');
+    calculator.numberClick(5);
+    calculator.operatorClick('=');
+    const actual = calculator.runningTotal;
+    assert.strictEqual(actual, expected);
+  })
+
+  it('should perform the previous subtraction operation when an operator button is clicked', function(){
+    const expected = 4;
+    calculator.numberClick(6);
+    calculator.operatorClick('-');
+    calculator.numberClick(2);
+    calculator.operatorClick('=');
+    const actual = calculator.runningTotal;
+    assert.strictEqual(actual, expected);
+  })
+
+  it('should perform the previous multiplication operation when an operator button is clicked', function(){
+    const expected = 12;
+    calculator.numberClick(6);
+    calculator.operatorClick('*');
+    calculator.numberClick(2);
+    calculator.operatorClick('=');
+    const actual = calculator.runningTotal;
+    assert.strictEqual(actual, expected);
+  })
+
+  it('should perform the previous divide operation when an operator button is clicked', function(){
+    const expected = 3;
+    calculator.numberClick(6);
+    calculator.operatorClick('/');
+    calculator.numberClick(2);
+    calculator.operatorClick('=');
+    const actual = calculator.runningTotal;
+    assert.strictEqual(actual, expected);
+  })
+
+  it('should clear the previous operator when the = operator button is clicked', function(){
+    const expected = null;
+    calculator.numberClick(6);
+    calculator.operatorClick('*');
+    calculator.numberClick(2);
+    calculator.operatorClick('=');
+    const actual = calculator.previousOperator;
+    assert.strictEqual(actual, expected);
+  })
+
+  it('should set the previous operator to + when the + operator button is clicked', function(){
+    const expected = '+';
+    calculator.numberClick(6);
+    calculator.operatorClick('+');
+    const actual = calculator.previousOperator;
+    assert.strictEqual(actual, expected);
+  })
+
+  it('should set the previous operator to - when the - operator button is clicked', function(){
+    const expected = '-';
+    calculator.numberClick(6);
+    calculator.operatorClick('-');
+    const actual = calculator.previousOperator;
+    assert.strictEqual(actual, expected);
+  })
+
+  it('should set the previous operator to * when the * operator button is clicked', function(){
+    const expected = '*';
+    calculator.numberClick(6);
+    calculator.operatorClick('*');
+    const actual = calculator.previousOperator;
+    assert.strictEqual(actual, expected);
+  })
+
+  it('should set the previous operator to / when the / operator button is clicked', function(){
+    const expected = '/';
+    calculator.numberClick(6);
+    calculator.operatorClick('/');
+    const actual = calculator.previousOperator;
+    assert.strictEqual(actual, expected);
+  })
+
 
 });
